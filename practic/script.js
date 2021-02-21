fe955b7da65865350766038abc8ef37dd58d4197
tre3ubec <ekoroteev27@yandex.ru>
2021-02-21 13:20:03 +0300

    finally practicum

--- practic/script.js
+++ practic/script.js
@@ -1,9 +1,31 @@
-let width = window.innerWidth
+
 let menu = document.querySelector(".menu")
-let burgerIcon = document.querySelector(".burger")
+let wrapper = document.querySelector('.wrapper')
+let activity = false
 
 
+function burgerMenu() {
+    let navbar = document.querySelector('.navbar')
+    let burger = `<div style="height: 40%;" class="adaptiveMenu wrapper">
+  <div style="display: grid;" class="menu">
+    <a class="menu-link" href="#"><span>Starting</span></a>
+    <a class="menu-link" href="#"><span>How to</span></a>
+    <a class="menu-link" href="#"><span>FAQ</span></a>
+    <a class="menu-link" href="#"><span>Forum</span></a>
+    <a class="menu-link" href="#"><span>Pattern</span></a>
+  </div>
+</div>`
+    let adaptiveMenu = document.querySelector('.adaptiveMenu')
+    if (activity === false) {
+        activity = true
+        return navbar.insertAdjacentHTML('beforebegin', burger)
+    } else if (activity === true) {
+        activity = false
+        return adaptiveMenu.parentNode.removeChild(adaptiveMenu)
+
+    }
+
+}
 
 
-
-console.log(width)
+document.addEventListener('click', burgerMenu)
